b59c32e06dd74da2e4b88a11f7faa09a7783588a
utopia-developer <utopia0developer@gmail.com>
2019-02-07 22:35:20 +0800

    完成自定义数组类对泛型的支持

--- src/chapter1/array/MyArray.java
+++ src/chapter1/array/MyArray.java
@@ -4,11 +4,11 @@ package chapter1.array;
  * @author utopia
  * 自定义数组类，实现“动态”数组
  */
-public class MyArray {
+public class MyArray<T> {
     /**
      * 自定义的数组
      */
-    private int[] data;
+    private T[] data;
     /**
      * 数组的有效元素个数
      */
@@ -20,7 +20,7 @@ public class MyArray {
      * @param capacity 数组容量
      */
     public MyArray(int capacity) {
-        data = new int[capacity];
+        data = (T[])new Object[capacity];
         size = 0;
     }
 
@@ -63,7 +63,7 @@ public class MyArray {
      *
      * @param element 添加的元素
      */
-    public void addLast(int element) {
+    public void addLast(T element) {
         add(size, element);
     }
 
@@ -72,7 +72,7 @@ public class MyArray {
      *
      * @param element 添加的元素
      */
-    public void addFirst(int element) {
+    public void addFirst(T element) {
         add(0, element);
     }
 
@@ -82,7 +82,7 @@ public class MyArray {
      * @param index   指定位置的索引
      * @param element 添加的元素
      */
-    public void add(int index, int element) {
+    public void add(int index, T element) {
         if (size == data.length) {
             throw new IllegalArgumentException("add() failed, data array is full.");
         }
@@ -102,8 +102,8 @@ public class MyArray {
      * @param index 指定的索引
      * @return 索引所在位置的元素
      */
-    public int getElement(int index) {
-        if (index < 0 || index > size) {
+    public T getElement(int index) {
+        if (index < 0 || index >= size) {
             throw new IllegalArgumentException("getElement() failed, index is illegal.");
         }
         return data[index];
@@ -115,8 +115,8 @@ public class MyArray {
      * @param index   指定的索引
      * @param element 更新的元素
      */
-    public void setElement(int index, int element) {
-        if (index > size) {
+    public void setElement(int index, T element) {
+        if (index >= size) {
             throw new IllegalArgumentException("setElement() failed, index is illegal.");
         } else if (index < 0) {
             throw new IllegalArgumentException("setElement() failed, index is illegal.");
@@ -130,9 +130,9 @@ public class MyArray {
      * @param value 需要判断的值
      * @return 该值存在与否
      */
-    public boolean contains(int value) {
+    public boolean contains(T value) {
         for (int i = 0; i < size; i++) {
-            if (data[i] == value) {
+            if (data[i].equals(value)) {
                 return true;
             }
         }
@@ -146,9 +146,9 @@ public class MyArray {
      * @param value
      * @return 若存在该元素则返回相应的索引，若不存在该元素则返回-1
      */
-    public int find(int value) {
+    public int find(T value) {
         for (int i = 0; i < size; i++) {
-            if (data[i] == value) {
+            if (data[i].equals(value)) {
                 return i;
             }
         }
@@ -161,15 +161,18 @@ public class MyArray {
      * @param index 指定的索引
      * @return 返回删除的元素，实现为用户备份此元素
      */
-    public int delete(int index) {
-        if (index < 0 || index > size) {
+    public T delete(int index) {
+        if (index < 0 || index >= size) {
             throw new IllegalArgumentException("delete() failed, index is illegal.");
         }
-        int removeValue = data[index];
+        T removeValue = data[index];
         for (int i = index + 1; i < size; i++) {
             data[i - 1] = data[i];
         }
         size--;
+        //使用泛型后添加此语句，释放引用，从而交由Java自动回收机制处理，清理内存（非必须语句）
+        // loitering objects ！= memory leak
+        data[size] = null;
         return removeValue;
     }
 
@@ -178,7 +181,7 @@ public class MyArray {
      *
      * @return 删除的元素
      */
-    public int deleteFirst() {
+    public T deleteFirst() {
         return delete(0);
     }
 
@@ -187,7 +190,7 @@ public class MyArray {
      *
      * @return 删除的元素
      */
-    public int deleteLast() {
+    public T deleteLast() {
         return delete(size - 1);
     }
 
@@ -197,7 +200,7 @@ public class MyArray {
      *
      * @param value 要删除的值
      */
-    public void deleteElement(int value) {
+    public void deleteElement(T value) {
         int index = find(value);
         if (index != -1) {
             delete(index);
--- src/chapter1/array/MyArrayTest.java
+++ src/chapter1/array/MyArrayTest.java
@@ -2,7 +2,7 @@ package chapter1.array;
 
 public class MyArrayTest {
     public static void main(String[] args) {
-        MyArray array = new MyArray(20);
+        MyArray<Integer> array = new MyArray<>(20);
         for (int i = 0; i < 10; i++) {
             array.addLast(i);
         }
@@ -19,14 +19,5 @@ public class MyArrayTest {
         System.out.println(array);
         array.deleteLast();
         System.out.println(array);
-        System.out.println("***********************************");
-        MyArray arr = new MyArray(3);
-        arr.setElement(0, 1);
-//        arr.setElement(1, 0);
-//        arr.setElement(2, 1);
-        System.out.println(arr);
-        System.out.println(arr.find(1));
-        arr.deleteElement(1);
-        System.out.println(arr);
     }
 }
